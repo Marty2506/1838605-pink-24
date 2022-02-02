7f49d090ca52199f25395fa67c7dc07b986945dc
Marty <Marty2009@yandex.ru>
2022-02-02 18:30:15 +0300

    переделал фон, добавил работающую менюшку без js, карту без js

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,9 +1,13 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var pageHeader = document.querySelector('.page-header');
 
+pageHeader.classList.remove('page-header--no-js');
 navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('main-nav__toggle--no-js');
 navMain.classList.add('main-nav--closed');
 navToggle.classList.add('toggle--closed');
+navToggle.classList.remove('main-nav__toggle--no-js');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
